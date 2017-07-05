6dc73759214812dd9194fd9d0d24ec272d5de4a7
Miller Adams <milleradams@hotmail.co.nz>
2017-07-05 14:04:48 +1200

    Sets up Piece pool as a virtual peice tracker

--- src/com/company/GamePiece.java
+++ src/com/company/GamePiece.java
@@ -5,10 +5,14 @@ package com.company;
  * A single game peice
  */
 public class GamePiece {
+
+    // All pieces are interchangable, movement is currently handled by the track
+    // so only current requirement is an ID.
     static private int count = 0;
     private int ID;
 
     public GamePiece(){
+
         this.ID = count++;
     }
 
--- src/com/company/PiecePool.java
+++ src/com/company/PiecePool.java
@@ -3,7 +3,44 @@ package com.company;
 /**
  * Created by Main on 05-Jul-17.
  * This keeps track of the Pool of unused tokens that have yet to be place on the board.
+ * As all pieces are interchangable this currently stores them just as ints.
  *
+ * Primary interactions should be throgh the BoardTrack Class
  */
 public class PiecePool {
+
+    private int numPieces;
+    private int rightPool;
+
+    // Constructors
+    public PiecePool(){
+        // By default pools should be empty.
+        this.numPieces = 0;
+
+    }
+
+    public  PiecePool(int numPieces){
+        // Most game modes will have even pools
+        this.rightPool = numPieces;
+    }
+
+
+    public void spawnPiece(){
+        //Creates a virtual piece by incrementing one side.
+        //In future this could be amended to actually create a GamePiece object
+
+        numPieces++;
+
+    }
+    public void killPiece(){
+        //coutnerpart to spawn Piece
+
+        numPieces--;
+
+    }
+
+    public int getSize(){
+        return this.numPieces;
+    }
+
 }
