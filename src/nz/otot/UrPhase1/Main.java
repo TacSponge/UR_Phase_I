f3138f3cf4f82caad94a24ebc904d51f610414a5
Miller Adams <milleradams@hotmail.co.nz>
2018-01-30 11:12:57 +1300

    Implements UI and creates first possible display

---
+++ src/nz/otot/UrPhase1/Main.java
@@ -0,0 +1,14 @@
+package nz.otot.UrPhase1;
+
+import nz.otot.UrPhase1.UI.*;
+import nz.otot.UrPhase1.model.*;
+
+public class Main {
+
+    public static Boolean testing = true;
+
+    public static void main(String[] args) {
+        Interactor state = new GameState();
+        UI.startUI(state);
+    }
+}
